8e8f624bda60ae0884b2e243009241b3432091fb
Miraj <miraj@mislbd.com>
2022-12-26 10:54:16 +0600

    initail

---
+++ src/main/java/com/learning/controller/HelloController.java
@@ -0,0 +1,22 @@
+package com.learning.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping(value = "/greeting", method = RequestMethod.GET)
+    public String greeting(@RequestParam(value = "name", required = false, defaultValue = "World") String name, ModelMap modelMap){
+
+        // add attribute to load modelMap
+        modelMap.addAttribute("greet",
+                "Hello " + name);
+
+        // return the name of the file to be loaded "hello_world.jsp"
+        return "greeting";
+    }
+}
---
+++ src/main/webapp/WEB-INF/jsp/greeting.jsp
@@ -0,0 +1,9 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Spring WEB MVC Demo</title>
+</head>
+<body>
+    <h2>${greet}</h2>
+</body>
+</html>
